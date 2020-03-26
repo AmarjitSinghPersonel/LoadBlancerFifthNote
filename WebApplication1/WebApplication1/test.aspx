fa7348faa3b6f1ff3fcfd8f7de781efc1afd4053
AmarjitSinghPersonel <40385884+AmarjitSinghPersonel@users.noreply.github.com>
2020-03-26 14:43:29 +0530

    1st commit

---
+++ WebApplication1/WebApplication1/test.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="test.aspx.cs" Inherits="WebApplication1.test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server" ID="txtUsers"></asp:TextBox>
+            <asp:TextBox runat="server" ID="txtTask"></asp:TextBox>
+            <asp:Button runat="server" ID="btnSubmit" OnClick="btnSubmit_Click" />
+            <asp:Label runat="server" ID="lblOutput"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/test.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class test : System.Web.UI.Page
+    {
+        bool taskAssigned = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class clsAsignedTask
+        {
+            public int user { get; set; }
+            public double tasks { get; set; }
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            int usr = Convert.ToInt32(txtUsers.Text);
+            double task = Convert.ToDouble(txtTask.Text);
+            List<clsAsignedTask> lstOutput = new List<clsAsignedTask>();
+            lstOutput.Add(new clsAsignedTask { user = 1, tasks = 4 });
+            lstOutput.Add(new clsAsignedTask { user = 2, tasks = 5 });
+            lstOutput.Add(new clsAsignedTask { user = 3, tasks = 1 });
+            var totaltsk = 10;
+            taskAssigned = true;
+            if (!taskAssigned)
+            {
+                var tskAss = task / usr;
+                lblOutput.Text = "Tasks Assigend per user=" + Convert.ToString(tskAss);
+                taskAssigned = true;
+
+                for (int i = 0; i <= usr; i++)
+                {
+                    lstOutput.Add(new clsAsignedTask { user = i, tasks = tskAss });
+                }
+            }
+            else
+            {
+                var totaltask = totaltsk + task;
+                var tskAss = Math.Round(totaltask / usr);
+                int j = 1;
+                foreach (var itm in lstOutput)
+                {
+                    if (itm.tasks > tskAss)
+                    {
+                        var newTask = itm.tasks - tskAss;
+                        lstOutput.Where(a => a.user == j).FirstOrDefault().tasks = Math.Round(itm.tasks - newTask);
+                    }
+                    else if (itm.tasks < tskAss)
+                    {
+                        var newTask = tskAss - itm.tasks;
+                        lstOutput.Where(a => a.user == j).FirstOrDefault().tasks = Math.Round(itm.tasks + newTask);
+                    }
+                    else
+                    {
+
+                    }
+                    j++;
+                }
+                lblOutput.Text = "Tasks Assigend per user=" + Convert.ToString(lstOutput[0].tasks);
+
+            }
+        }
+    }
+}
